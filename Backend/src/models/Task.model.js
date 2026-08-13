5bac1459ab476939ae012931fdb49cc6db1eeafd
princeranaa <ranaprince45may@gmail.com>
2026-08-13 21:38:12 +0530

    remove validation from model

--- Backend/src/models/Task.model.js
+++ Backend/src/models/Task.model.js
@@ -4,27 +4,23 @@ const taskSchema = new mongoose.Schema(
   {
     title: {
       type: String,
-      required: [true, "Task title is required"],
+
       trim: true,
-      minlength: [3, "Task title must be at least 3 characters"],
-      maxlength: [100, "Task title cannot exceed 100 characters"],
     },
 
     description: {
       type: String,
-      required: [true, "Task description is required"],
+
       trim: true,
     },
 
     assignedTo: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: [true, "Assigned employee is required"],
     },
 
     dueDate: {
       type: Date,
-      required: [true, "Due date is required"],
     },
 
     priority: {
