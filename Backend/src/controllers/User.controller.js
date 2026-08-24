5f067327e5e5bf1a38a3aea41c0cfe79990c01d7
princeranaa <ranaprince45may@gmail.com>
2026-08-24 23:30:15 +0530

    feat: implement Logout API

--- Backend/src/controllers/User.controller.js
+++ Backend/src/controllers/User.controller.js
@@ -95,14 +95,30 @@ export const login = async (req, res) => {
 export const getMe = async (req, res) => {
   try {
     const user = req.user;
-    await userModel.findById(user);
     return res.status(200).json({
       message: "User fetched successfully",
       user: user,
     });
   } catch (error) {
     console.log("error", error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
   }
 };
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.status(200).json({
+      message: "User logged out successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
 
 
--- Backend/src/routes/User.routes.js
+++ Backend/src/routes/User.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { registerValidation,loginValidation } from "../Middlewares/Validation.middleware.js";
-import { register,login, getMe } from "../controllers/User.controller.js";
+import { registerValidation, loginValidation } from "../Middlewares/Validation.middleware.js";
+import { register, login, getMe, logout } from "../controllers/User.controller.js";
 import { authMiddleware } from "../Middlewares/Auth.middleware.js";
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post("/register", registerValidation, register);
 router.post("/login", loginValidation, login);
 router.get("/profile", authMiddleware, getMe);
-
+router.post("/logout", logout);
 
 export default router;
+
